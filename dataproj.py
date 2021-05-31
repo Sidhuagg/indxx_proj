1e3e10cedea069f3511327b93c8ea6ad9bdfd451
Sidhuagg <40688586+Sidhuagg@users.noreply.github.com>
2021-06-01 01:45:04 +0530

    Update dataproj.py

--- dataproj.py
+++ dataproj.py
@@ -1,29 +1,38 @@
+from django.core.management.base import BaseCommand, CommandError
+from dashboard.models import airdata as tdata
 import requests
 import json
-import datetime
-from _tkinter import *
-import time
-#for a in range(0,1578,10):
-a = True
-offno = 0
-lst1=[]
-while a == True:
-    api_key = "579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b"
-    url = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key={}&format=json&offset={}&limit=10".format(api_key,offno)
-    response = requests.get(url)
-    data = response.text
-    a1=json.loads(data)
-    for x in a1['records']:
-        lst1.append(x)
-    if a1["count"] < 10:
-        a= False
-    offno += 10
-    print(offno)
-airdata1 = json.loads(data)
-airdata1['records']=lst1
-airdata = json.dumps(airdata1,indent=2)
-with open("air.txt", "a") as file:
 
-    file.write(airdata)
+class Command(BaseCommand):
+    help = 'Fetches api data'
+
+    """def add_arguments(self, parser):
+        none"""
 
 
+    def handle(self, *args, **options):
+
+        #for a in range(0,1578,10):
+        a = True
+        offno = 0
+        lst1=[]
+        while a == True:
+            api_key = "579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b"
+            url = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key={}&format=json&offset={}&limit=10".format(api_key,offno)
+            response = requests.get(url)
+            data = response.text
+            a1=json.loads(data)
+            for x in a1['records']:
+                lst1.append(x)
+            if a1["count"] < 10:
+                a= False
+            offno += 10
+        airdata1 = json.loads(data)
+        airdata1['records']=lst1
+        ad = json.dumps(airdata1,indent=2)
+        tdata.objects.bulk_create(lst1)
+        return ad
+        tdata.objects.bulk_create(lst1)
+        """with open("air.txt", "a") as file:
+
+            file.write(airdata)"""
