3abfab05a23ed8f09497b31fcb7864d634febed4
Sidhuagg <40688586+Sidhuagg@users.noreply.github.com>
2021-06-03 05:44:46 +0530

    Update modelairdata.py

--- modelairdata.py
+++ modelairdata.py
@@ -2,19 +2,12 @@ from django.db import models
 
 # Create your models here.
 class date_time(models.Model):
-    last_update = models.CharField(max_length=100)
-
-
+    last_update = models.DateTimeField()
 
 class Pollutant(date_time):
-    pollutant_id = models.CharField(max_length=50)
-    pollutant_min = models.IntegerField()
-    pollutant_max = models.IntegerField()
-    pollutant_avg = models.IntegerField()
+    poll = models.CharField(max_length=100, null = True)
 
 class airdata(models.Model):
-    country = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    station = models.CharField(max_length=100)
+    #id = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
     pollutant = models.ForeignKey(Pollutant, on_delete=models.CASCADE)
